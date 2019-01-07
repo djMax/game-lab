cb651bcfcfa8ce7ea7c199590c493a183c2abb2c
Max Metral <max@pyralis.com>
2019-01-07 14:45:10 -0500

    Fix relative paths in the right place

--- server/index.js
+++ server/index.js
@@ -12,10 +12,10 @@ io.on('connection', (client) => {
 });
 
 // This stuff serves the built React app in production
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, '..', 'build')));
 
 app.get('*', function(req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, '..', 'build', 'index.html'));
 });
 
 const port = process.env.PORT || 8000
